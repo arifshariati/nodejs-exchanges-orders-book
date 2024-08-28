f286b6de282efee19efe476742b5920c98204c0e
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-08-28 20:40:23 +0200

    feat: initialize prices module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PricesModule } from './modules/prices/prices.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PricesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
